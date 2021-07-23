f0f3763b39ae142cfd53e68a7be3207089f57971
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-07-23 15:04:32 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,29 @@
-console.log("ml5 version:",ml5.version);
+var images=["pencil.png","knife.jpeg","binder.jpeg","blender.jpeg","penstand.jpg","car.jpeg"];
+var azzure_result=["eyeliner pencil","chef knife","document box","blender","pen stand","the oil war"];
+var mobile_result=["crane","cleaver","binder"," pencil sharpener","file","sports car"]
+var conclusion_result=["None of them are accurate","Google lens is more accurate than MobileNet","MobileNet is more accurate than Google Lens","Google lens WAY  MORE is more accurate than MobileNet","Google lens WAY MORE is more accurate than MobileNet","MobileNet is WAY MORE more accurate than Google Lens"];
+var image_no= 0;
 
-noseX= 0;
-noseY= 0;
-difference=0;
-leftWrist= 0;
-rightWrist=0;
- 
-function preload(){
-
+function change_image(){
+    image_no +=1;
+if(image_no==6){
+image_no=0;
+document.getElementById("test_image").src= images[image_no];
+}
+else{
+document.getElementById("test_image").src= images[image_no];
+}
 }
 
-function setup(){
-    canvas= createCanvas(500,500);
-    canvas.position(700,210);
-    video= createCapture(VIDEO);
-    video.size(550,500);
-    video.position(0,210);
-    posenet= ml5.poseNet(video, modelLoaded);
-    posenet.on('pose',gotPoses);
-}
-
-function draw(){
-    document.getElementById("result").innerHTML= "Width and Height of the square is: "+difference+" pixels";
-    background("#969A97");
-    stroke("#B4846C");
-    fill("#B4846C");
-square(noseX,noseY,difference);
-}
-
-function modelLoaded(){
-    console.log("posenet is loaded :D")
-}
-
-function gotPoses(result){
-    if(result.length>0){
-        console.log(result);
-        noseX= result[0].pose.nose.x;
-        noseY= result[0].pose.nose.y;
-        leftWrist= result[0].pose.leftWrist.x;
-        rightWrist= result[0].pose.rightWrist.x;
-        difference= abs(floor(leftWrist-rightWrist));
-    }
+function check(){
+    azzure_results= azzure_result[image_no];
+    mobilenet_results= mobile_result[image_no];
+    conclusion_results= conclusion_result[image_no];
+    console.log(azzure_result);
+document.getElementById("azzure_results_div").style.visibility= "visible";
+document.getElementById("mobilenet_results_div").style.visibility= "visible";
+document.getElementById("conclusion_results_div").style.visibility= "visible";
+document.getElementById("wolfram_results").innerHTML= azzure_results;
+document.getElementById("mobilenet_results").innerHTML= mobilenet_results;
+document.getElementById("conclusion_results").innerHTML= conclusion_results;
 }
